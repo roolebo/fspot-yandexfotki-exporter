46dacdf58c9f6d7c09739ee659603ef8e9352dde
Роман <wisecapt@wisecapt-netbook.(none)>
2010-01-26 14:35:26 +0300

    trying to upload new file

---
+++ yandex-sharp/Authentication.cs
@@ -0,0 +1,37 @@
+using System;
+using Mono.Math;
+
+namespace Mono.Yandex.Fotki {
+    class Authentication {
+        private class PublicKey {
+            private BigInteger module, exponent;
+
+            PublicKey (string key)
+            {
+                string[] key_pair;
+                key_pair = key.Split (new[] {'#'}, 2);
+                if (key_pair!=2)
+                    throw new ArgumentException ("Incorrect value", "key");
+
+                module = ParseHex(key_pair[0]);
+                exponent = ParseHex(key_pair[1]);
+            }
+
+            private static BigInteger ParseHex (string hexString)
+            {
+                BigInteger result = new BigInteger();
+                foreach (char c in hexString) {
+                    result = result * 16 + int.Parse(new c.ToString(), 
+                            System.Globalization.NumberStyles.AllowHexSpecifier);
+                }
+
+                return result;
+            }
+
+            public BigInteger Encrypt(BigInteger plain)
+            {
+                return plain.ModPow(exponent, module);
+            }
+        }
+    }
+}

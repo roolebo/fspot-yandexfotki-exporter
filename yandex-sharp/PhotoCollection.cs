acf3d06580788c4b31b61e12bc205e6cfc0eec7f
Maxim Kolchin <kolchinmax@gmail.com>
2010-01-28 16:42:29 +0300

    Added new class: PhotoCollection.

---
+++ yandex-sharp/PhotoCollection.cs
@@ -0,0 +1,23 @@
+
+using System;
+using System.Collections;
+
+namespace Mono.Yandex.Fotki{
+	
+	public class PhotoCollection{
+		private ArrayList photos;
+
+		public PhotoCollection ()
+		{
+		}
+		
+		public void Add (Photo photo)
+		{}
+		
+		public void Edit (Photo photo)
+		{}
+		
+		public void Delete (Photo photo)
+		{}
+	}
+}

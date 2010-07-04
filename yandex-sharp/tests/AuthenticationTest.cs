a643fd7e00556e0ee48fcc8e674da7a0ce64dbb2
Roman Bolshakov <roolebo@gmail.com>
2010-07-04 17:03:56 +0400

    add authentication test with two cases

---
+++ yandex-sharp/tests/AuthenticationTest.cs
@@ -0,0 +1,37 @@
+namespace Mono.Yandex.Fotki {
+        using System;
+        using System.IO;
+        using NUnit.Framework;
+
+        [TestFixture]
+        public class AuthenticationTest {
+                String[] login_config = new String[2];
+
+                [SetUp]
+                public void Init ()
+                {
+                        //the first line of config file is the login, 
+                        //and the second one is the password
+                        using (StreamReader sr = new StreamReader ("config")) {   
+                                login_config[0] = sr.ReadLine ();
+                                login_config[1] = sr.ReadLine ();
+                        }
+                }
+
+                [Test]
+                public void GetToken ()
+                {
+                        String token = Authentication.GetAuthorizationToken 
+                                (login_config[0], login_config[1]);
+                        Assert.AreNotEqual(token, null);
+                }
+                
+                [Test]
+                public void GetTokenWithoutPassword ()
+                {
+                        String token = Authentication.GetAuthorizationToken
+                                (login_config[0], null);
+                        Assert.AreEqual(token, null);
+                }
+        }
+}
